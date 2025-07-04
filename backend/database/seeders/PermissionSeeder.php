7a305ede273682f52db6be2c4fe5349f1e616f15
Asdhi Maulana Hidayatullah <asdhibadi@gmail.com>
2025-07-04 15:13:29 +0700

    adding complete permission

--- backend/database/seeders/PermissionSeeder.php
+++ backend/database/seeders/PermissionSeeder.php
@@ -1,6 +1,7 @@
 <?php
-namespace App\Models;
+
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use App\Models\Permission;
 
@@ -10,25 +11,29 @@ class PermissionSeeder extends Seeder
     {
         // Daftar izin berdasarkan menu di aplikasi Anda
         $permissions = [
-            ['name' => 'Dashboard Access Page', 'slug' => 'view_dashboard'],
-            ['name' => 'Account Control Page', 'slug' => 'manage_account_control'],
-            ['name' => 'Work Order Page', 'slug' => 'work_order_page'],
-            ['name' => 'Bill of Material Page', 'slug' => 'bill_of_material_page'],
-            ['name' => 'Customers Page', 'slug' => 'customers_page'],
-            ['name' => 'Purchase Request Page', 'slug' => 'purchase_request_page'],
-            ['name' => 'Purchase Order Page', 'slug' => 'purchase_order_page'],
-            ['name' => 'Suppliers Page', 'slug' => 'suppliers_page'],
-            ['name' => 'Price List Page', 'slug' => 'price_list_page'],
-            ['name' => 'Master Parts Page', 'slug' => 'master_parts_page'],
-            ['name' => 'Incoming Page', 'slug' => 'incoming_page'],
-            ['name' => 'Outgoing Page', 'slug' => 'outgoing_page'],
-            ['name' => 'Return Page', 'slug' => 'return_page'],
-            ['name' => 'Stock Adjustment Page', 'slug' => 'stock_adjustment_page'],
-            ['name' => 'Balance Stock Page', 'slug' => 'balance_stock_page'],
+            ['name' => 'Akses Dasbor', 'slug' => 'view_dashboard'],
+            ['name' => 'Manajemen Akun', 'slug' => 'manage_account_control'],
+            ['name' => 'Halaman Work Order', 'slug' => 'work_order_page'],
+            ['name' => 'Halaman Bill of Material', 'slug' => 'bill_of_material_page'],
+            ['name' => 'Halaman Customer', 'slug' => 'customers_page'],
+            ['name' => 'Halaman Purchase Request', 'slug' => 'purchase_request_page'],
+            ['name' => 'Halaman Purchase Order', 'slug' => 'purchase_order_page'],
+            ['name' => 'Halaman Supplier', 'slug' => 'suppliers_page'],
+            ['name' => 'Halaman Price List', 'slug' => 'price_list_page'],
+            ['name' => 'Halaman Master Part', 'slug' => 'master_parts_page'],
+            ['name' => 'Halaman Incoming', 'slug' => 'incoming_page'],
+            ['name' => 'Halaman Outgoing', 'slug' => 'outgoing_page'],
+            ['name' => 'Halaman Return', 'slug' => 'return_page'],
+            ['name' => 'Halaman Stock Adjustment', 'slug' => 'stock_adjustment_page'],
+            ['name' => 'Halaman Balance Stock', 'slug' => 'balance_stock_page'],
         ];
 
         foreach ($permissions as $permission) {
-            Permission::create($permission);
+            // Gunakan updateOrCreate untuk menghindari duplikasi
+            Permission::updateOrCreate(
+                ['slug' => $permission['slug']], // <-- Kondisi untuk mencari
+                ['name' => $permission['name']]  // <-- Nilai untuk di-update atau dibuat
+            );
         }
     }
 }
